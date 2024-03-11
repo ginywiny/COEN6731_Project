481b74e5a9e3ace56901186b8ca7b400d6306ca7
michael <michaelles97@gmail.com>
2024-03-11 01:29:10 -0400

    Update with production thread and request counts

--- SkiClient/ski_client/src/main/java/com/example/SkiClient.java
+++ SkiClient/ski_client/src/main/java/com/example/SkiClient.java
@@ -5,8 +5,8 @@ public class SkiClient {
     public static void main(String[] args) {
 
         // ClientRunnable client = new ClientRunnable(1, 1, 1, 5);
-        ClientRunnable client = new ClientRunnable(10, 10, 10, 5);
-        // ClientRunnable client = new ClientRunnable(32, 1000, 10000, 5);
+        // ClientRunnable client = new ClientRunnable(10, 10, 10, 5);
+        ClientRunnable client = new ClientRunnable(32, 1000, 10000, 5);
         client.run();
 
     }
